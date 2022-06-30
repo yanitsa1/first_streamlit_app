40942ff5066655393ac342edf196fa77678e394f
Yanitsa1 <108458140+yanitsa1@users.noreply.github.com>
2022-06-30 12:30:53 +0300

    Update streamlit_app.py
    Fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🫐🍚 Omega 3 & Bluberry Oatmeal')
 streamlit.text('🥬Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🥪 Avocado Toast'
+streamlit.text('🥑🥪 Avocado Toast')
                
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
