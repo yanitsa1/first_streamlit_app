0bbf31438c209572409afca70eea2c8fb3ec8d62
Yanitsa1 <108458140+yanitsa1@users.noreply.github.com>
2022-06-30 12:13:25 +0300

    Update streamlit_app.py
    Add more text to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Dinner')
+streamlit.text('Breakfast Menu')
+stremalit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
