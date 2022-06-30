4982ba255ab414119e316a7985c845e9e47e616c
Yanitsa1 <108458140+yanitsa1@users.noreply.github.com>
2022-06-30 12:30:03 +0300

    Update streamlit_app.py
    Update the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 
-streamlit.title('My Parents new Healthy Dinner')
+streamlit.title('My Mom\'s new Healthy Dinner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🫐🍚 Omega 3 & Bluberry Oatmeal')
+streamlit.text('🥬Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🥪 Avocado Toast'
+               
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
