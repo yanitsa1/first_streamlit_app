7e71fd3025bc20538ed81e7b3092a46a7591dca7
Yanitsa1 <108458140+yanitsa1@users.noreply.github.com>
2022-07-05 08:27:58 +0300

    Update streamlit_app.py
    import snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,4 +35,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display the table on the page.
 streamlit.dataframe(fruityvice_normalized)
 
-
+import snowflake.connector
