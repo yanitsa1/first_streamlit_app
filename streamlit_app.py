ede884893c88a833f94d574ca55016fc4489a7df
Yanitsa1 <108458140+yanitsa1@users.noreply.github.com>
2022-07-06 09:04:33 +0300

    Update streamlit_app.py
    close the connection at the end of each button's script.Finally, let's change a title and a button label. These just make a little more sense.

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ try:
 except URLError as e:
     streamlit.error()
 
-streamlit.header("The fruit list contains:")
+streamlit.header("View Our Fruit List - Add Your Fvorites!")
 # Snowflake related functions
 def get_fuit_load_list():
     with my_cnx.cursor() as my_cur:
@@ -52,9 +52,10 @@ def get_fuit_load_list():
          return my_cur.fetchall()
         
 # Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fuit_load_list()
+   my_snx.close()
    streamlit.dataframe(my_data_rows)
     
 # Allow the end user to add a fruit to the list
@@ -67,6 +68,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])  
     back_from_function = insert_row_snowflake(add_my_fruit)
+    my_snx.close()
     streamlit.text(back_from_function)
     
 
